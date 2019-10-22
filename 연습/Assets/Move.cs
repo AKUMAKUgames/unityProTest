38662f260eca24b913cd816f3f091aae0aa1f132
AKUMAKUgames <rlatjdghgood74@gmail.com>
2019-10-22 14:38:37 +0900

    Move 추가
    test

---
+++ 연습/Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float speed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed) ;
+        this.transform.Translate(Vector3.right* Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+    }
+}
+  
