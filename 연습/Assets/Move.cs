8e16b02092543187543803fd884e3385d8dc06fa
AKUMAKUgames <rlatjdghgood74@gmail.com>
2019-10-23 16:38:41 +0900

    움직임 효과 Test
    움직임 궤적 표시 효과

--- 연습/Assets/Move.cs
+++ 연습/Assets/Move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    float speed = 100.0f;
+    float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
