7cbb0ea3f4e3b3b45fe98ffc2bb74bc6b4b85f5d
tjddms622 <48954505+tjddms622@users.noreply.github.com>
2019-10-28 16:12:23 +0900

    총알,발사,기관총 에셋추가

---
+++ 연습/Assets/Fire.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Bullet;
+    public Transform FirePos;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //복제한다. //'Bullet'을 'FirePos.transform.position' 위치에 'FirePos.transform.rotation' 회전값으로.
+            Instantiate(Bullet, FirePos.transform.position, FirePos.transform.rotation);
+        }
+
+    }
+}
