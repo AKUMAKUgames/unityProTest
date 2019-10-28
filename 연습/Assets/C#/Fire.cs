45e0743be0c96e168446fbd223bccf79e4ede406
AKUMAKUgames <rlatjdghgood74@gmail.com>
2019-10-28 23:56:29 +0900

    카메라 무빙 및 플레이어 무빙,발사
    카메라 무빙은 플레이어의 위치를 따른다.
    플레이어의 회전은 키보드로 조작한다.
    무기는 연속발사가 가능하도록 지정

---
+++ 연습/Assets/C#/Fire.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Bullet;
+    public Transform FirePos;
+    public Animator ani;
+   
+    
+    bool shootlife = true; // 총알을 쓸 수 있는 상태다
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+         if (shootlife &&Input.GetMouseButton(0))
+        {
+           
+                
+
+                Instantiate(Bullet, FirePos.transform.position, FirePos.transform.rotation);
+                ani.Play("MachineGin_shoot");
+                
+               
+
+
+                // 잠깐 쉰다
+                shootlife = false;
+                Invoke("Sleep", 0.05f);
+            
+                
+         }
+        
+
+    }
+    void Sleep()
+    {
+       
+      
+        shootlife = true;
+    }
+}
---
+++ 연습/Assets/C#/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody myRigidbody;
+
+    public float rotationSpeed = 180f;
+    
+    public float speed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputRot = Input.GetAxis("Horizontal");
+        float inputSPD = Input.GetAxis("Vertical");
+
+        this.transform.Rotate(0, rotationSpeed * inputRot * Time.deltaTime, 0);
+        Vector3 moveDistance = transform.forward * speed * inputSPD * Time.deltaTime;
+
+        myRigidbody.MovePosition(myRigidbody.position + moveDistance);
+       /* this.transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed) ;
+        this.transform.Translate(Vector3.right* Input.GetAxis("Horizontal") * Time.deltaTime * speed);*/
+ 
+    }
+
+}
+  
