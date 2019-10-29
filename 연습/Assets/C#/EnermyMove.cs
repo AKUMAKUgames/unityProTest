4ee5e1358d107079c6138a032efcc3f1b3f15a8f
tjddms622 <48954505+tjddms622@users.noreply.github.com>
2019-10-29 14:18:03 +0900

    활성화
    비활성화

---
+++ 연습/Assets/C#/EnermyMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnermyMove : MonoBehaviour
+{
+    GameObject Player;
+    Vector3 dir;
+    public GameObject enermy;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        Player = GameObject.Find("Player");
+         dir = Player.transform.position - this.transform.position;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+           enermy.gameObject.SetActive(true);
+            Debug.Log("활성화");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+            enermy.gameObject.SetActive(false);
+            Debug.Log("비활성화");
+        }
+        dir = Player.transform.position - this.transform.position;
+        transform.position += dir * 0.50f * Time.deltaTime;
+        
+    }
+}
