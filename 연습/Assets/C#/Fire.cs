a3a6dda0438b55190207ad88385d462dbbfe297d
AKUMAKUgames <rlatjdghgood74@gmail.com>
2019-11-05 11:17:18 +0900

    탄창/탄약 재장전 구현
    ㅇㅇ 주석 친절하게 달았으니까
    fire 파일 보고 참고해

--- 연습/Assets/C#/Fire.cs
+++ 연습/Assets/C#/Fire.cs
@@ -9,6 +9,8 @@ public class Fire : MonoBehaviour
     public Transform FirePos;
     Animator ani;
      bool shootlife = true; // 총알을 쓸 수 있는 상태다
+    int bulletNumber = 30; // 1탄창에 들어있는 총알 수 (나중에 바꾸든가)
+    int bulletToT = 120; // 남은 탄약
 
 
     void Start()
@@ -20,12 +22,33 @@ public class Fire : MonoBehaviour
     void Update()
     {
         ani = this.GetComponent<Animator>();
-
-        if (shootlife && Input.GetMouseButton(0))
+        int reloadBullet = 30 - bulletNumber;//재장전하면 채워질 탄약 수 
+        if (Input.GetKeyDown(KeyCode.R))
         {
+            if (bulletNumber < 30 && bulletToT > 0) // 탄창이 풀이 아니고 잔탄있을 경우
+            {
+                if (bulletToT - reloadBullet >= 0) // 잔탄이 여유로울떄 
+                {
+                    bulletToT = bulletToT - reloadBullet;//재장전한 만큼 잔탄 감소
+                    bulletNumber = bulletNumber + reloadBullet;// 탄창 채워짐 
+                    Debug.Log("장전완료:" +bulletNumber+ " / "+ bulletToT);
+                }
+                else // 잔탄이 부족할때 
+                {
+                    bulletNumber += bulletToT;
+                    bulletToT = 0;
+                    Debug.Log("장전완료 풀충 ㄴㄴ:" +bulletNumber+ " / "+ bulletToT);
+                }
+            }
+            else { Debug.Log("총알 없어"); } //탄창이 풀이거나 잔탄이 없을 경우 
+        }
+        if (shootlife && Input.GetMouseButton(0) && bulletNumber>0) 
+        {
+           
             ani.enabled = true;
             Instantiate(Bullet, FirePos.transform.position, FirePos.transform.rotation);
-
+            bulletNumber = bulletNumber-1;
+            Debug.Log("히힛 총알 발싸:"+bulletNumber+ " / "+ bulletToT );
             ani.Play("MachineGin_shoot");
 
             // 잠깐 쉰다
