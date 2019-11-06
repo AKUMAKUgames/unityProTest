99e62e9c26115ccb8e3f9d735ff0cab2e03c903a
tjddms622 <48954505+tjddms622@users.noreply.github.com>
2019-11-06 18:20:35 +0900

    총,체력,데미지구현

--- 연습/Assets/C#/EnemyCollision.cs
+++ 연습/Assets/C#/EnemyCollision.cs
@@ -6,24 +6,41 @@ public class EnemyCollision : MonoBehaviour
 {
     
     Animation ani;
-    GameObject mon;
+    int health = 30;
+    
     // Start is called before the first frame update
     void Start()
     {
+      
         ani = GetComponent<Animation>();
     }
-
+ 
     // Update is called once per frame
-   
+   void Update()
+    {
+       
+    }
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Respawn")//간단하게 게임오브젝트를 판정
         {
-            Destroy(this.gameObject, 0.5f);
-            ani.Play("Anim_Death");
+            //player 태그로 가서 데미지 받아옴
+            int bulletPower = GameObject.FindWithTag("Player").GetComponent<PlayerStat>().damage;
+
             Destroy(col.gameObject);
-            Debug.Log("맞음");
+            health -= bulletPower;
+            
+            if (health <= 0)
+            {
+                Destroy(this.gameObject, 0.5f);
+                ani.Play("Anim_Death");
+            }
+
+            
+
+
         }
+        
     }
 
 }
---
+++ 연습/Assets/C#/PlayerStat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    public GameObject gun;
+    public GameObject machineGun;
+    public int damage = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            damage = 10;
+            gun.SetActive(true);
+            machineGun.SetActive(false);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            damage = 5;
+            gun.SetActive(false);
+            machineGun.SetActive(true);
+        }
+    }
+}
