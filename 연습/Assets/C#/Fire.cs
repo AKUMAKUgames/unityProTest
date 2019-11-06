b07c6c8dc9bcc0467386e53edca2b5b03ebb910b
AKUMAKUgames <rlatjdghgood74@gmail.com>
2019-11-06 17:06:01 +0900

    재장전 완전판
    재장전 모션 진행중 발사되는 버그 수정

--- 연습/Assets/C#/Fire.cs
+++ 연습/Assets/C#/Fire.cs
@@ -11,8 +11,7 @@ public class Fire : MonoBehaviour
      bool shootlife = true; // 총알을 쓸 수 있는 상태다
     int bulletNumber = 30; // 1탄창에 들어있는 총알 수 (나중에 바꾸든가)
     int bulletToT = 120; // 남은 탄약
-
-
+    bool loadTIme = true;
     void Start()
     {
         
@@ -24,16 +23,20 @@ public class Fire : MonoBehaviour
         ani = this.GetComponent<Animator>();
         int reloadBullet = 30 - bulletNumber;//재장전하면 채워질 탄약 수 
 
-
+        
+       
         if (Input.GetKeyDown(KeyCode.R))// R로 재장전
         {
             if (bulletNumber < 30 && bulletToT > 0) // 탄창이 풀이 아니고 잔탄있을 경우
             {
+                loadTIme = false;
+              
                 if (bulletToT - reloadBullet >= 0) // 잔탄이 여유로울떄 
-                {
+                {  
                     ani.enabled = true;
                     ani.Play("MachineGun_reload");
                     /* 입력을 2초간 받지 못하게 해야함 장전 모션 씹힘*/
+                    Invoke("SleepLoad", 2.0f);
                     bulletToT = bulletToT - reloadBullet;//재장전한 만큼 잔탄 감소
                     bulletNumber = bulletNumber + reloadBullet;// 탄창 채워짐 
                     Debug.Log("장전완료:" +bulletNumber+ " / "+ bulletToT);
@@ -41,17 +44,22 @@ public class Fire : MonoBehaviour
                 }
                 else // 잔탄이 부족할때 
                 { ani.enabled = true;
+                    
                     ani.Play("MachineGun_reload");
+
                     /* 입력을 2초간 받지 못하게 해야함 장전 모션 씹힘 */
+                    Invoke("SleepLoad", 2.0f);
                     bulletNumber += bulletToT;
                     bulletToT = 0;
                     Debug.Log("장전완료 풀충 ㄴㄴ:" +bulletNumber+ " / "+ bulletToT);
                    
                 }
+                
             }
             else { Debug.Log("총알 없어"); } //탄창이 풀이거나 잔탄이 없을 경우 
         }
-        if (shootlife && Input.GetMouseButton(0) && bulletNumber > 0)
+        
+        if (shootlife && Input.GetMouseButton(0) && bulletNumber > 0 && loadTIme)
         {
 
             ani.enabled = true;
@@ -81,5 +89,9 @@ public class Fire : MonoBehaviour
     void Sleep()
     {
         shootlife = true;
+    }
+    void SleepLoad()
+    {
+        loadTIme = true;
     }
 }
