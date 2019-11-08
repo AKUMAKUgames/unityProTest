f2832b8b4f3941000c8ecbf79d6103ec9c7a36bf
AKUMAKUgames <rlatjdghgood74@gmail.com>
2019-11-08 18:33:41 +0900

    악마와의 거래에서 졌다.
    Move 수정

---
+++ 연습/Assets/C#/move_2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_2 : MonoBehaviour
+{
+    public float speed = 5;
+    Camera cam;
+    move_3 move3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move3 = GetComponent<move_3>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 moveVel = moveInput.normalized * speed;
+        move3.Move(moveVel);
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (plane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            move3.rolling(point);
+        }
+    }
+}
