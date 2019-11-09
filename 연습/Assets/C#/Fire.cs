462704a4d4cfc5842e2085330dd4ffdb18543520
AKUMAKUgames <rlatjdghgood74@gmail.com>
2019-11-09 23:52:24 +0900

    롤백

--- 연습/Assets/C#/Fire.cs
+++ 연습/Assets/C#/Fire.cs
@@ -8,13 +8,18 @@ public class Fire : MonoBehaviour
     public GameObject Bullet;
     public Transform FirePos;
     Animator ani;
-     bool shootlife = true; // 총알을 쓸 수 있는 상태다
+
+    public AudioClip shot;
+    public AudioClip rel;
+
+    AudioSource Audio;
+    bool shootlife = true; // 총알을 쓸 수 있는 상태다
     int bulletNumber = 30; // 1탄창에 들어있는 총알 수 (나중에 바꾸든가)
     int bulletToT = 120; // 남은 탄약
     bool loadTIme = true;
     void Start()
     {
-        
+        Audio = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,29 +34,32 @@ public class Fire : MonoBehaviour
         {
             if (bulletNumber < 30 && bulletToT > 0) // 탄창이 풀이 아니고 잔탄있을 경우
             {
+                Audio.clip = rel;
                 loadTIme = false;
-              
+               ani.Play("MachineGun_reload");
                 if (bulletToT - reloadBullet >= 0) // 잔탄이 여유로울떄 
                 {  
                     ani.enabled = true;
-                    ani.Play("MachineGun_reload");
+                   
+                    Audio.Play();
                     /* 입력을 2초간 받지 못하게 해야함 장전 모션 씹힘*/
                     Invoke("SleepLoad", 2.0f);
-                    bulletToT = bulletToT - reloadBullet;//재장전한 만큼 잔탄 감소
-                    bulletNumber = bulletNumber + reloadBullet;// 탄창 채워짐 
+                  
                     Debug.Log("장전완료:" +bulletNumber+ " / "+ bulletToT);
-                    
+                      bulletToT = bulletToT - reloadBullet;//재장전한 만큼 잔탄 감소
+                    bulletNumber = bulletNumber + reloadBullet;// 탄창 채워짐 
                 }
                 else // 잔탄이 부족할때 
                 { ani.enabled = true;
                     
-                    ani.Play("MachineGun_reload");
-
+                  
+                    Audio.Play();
                     /* 입력을 2초간 받지 못하게 해야함 장전 모션 씹힘 */
                     Invoke("SleepLoad", 2.0f);
+                    
+                    Debug.Log("장전완료 풀충 ㄴㄴ:" +bulletNumber+ " / "+ bulletToT);
                     bulletNumber += bulletToT;
                     bulletToT = 0;
-                    Debug.Log("장전완료 풀충 ㄴㄴ:" +bulletNumber+ " / "+ bulletToT);
                    
                 }
                 
@@ -61,15 +69,16 @@ public class Fire : MonoBehaviour
         
         if (shootlife && Input.GetMouseButton(0) && bulletNumber > 0 && loadTIme)
         {
-
+            Audio.clip = shot;
             ani.enabled = true;
             GameObject go = Instantiate(Bullet, FirePos.transform.position, FirePos.transform.rotation) as GameObject;
             
             bulletNumber = bulletNumber - 1;
             if (bulletNumber == 0) { ani.enabled = false; }
             Debug.Log("히힛 총알 발싸:" + bulletNumber + " / " + bulletToT);
+            Audio.Play();
             ani.Play("MachineGin_shoot");
-
+            
             // 잠깐 쉰다
             shootlife = false;
             Invoke("Sleep", 0.05f);
